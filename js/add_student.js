14e8ed7eeca2230188fe9cc35f3b18b5fd8670fe
Ahmed Ashraf <loaiashraf285@gmail.com>
2023-04-17 23:43:43 +0200

    js

--- js/add_student.js
+++ js/add_student.js
@@ -1,78 +1,134 @@
-const form = document.getElementById('form');
-const Name = document.getElementById('Name');
-const Email = document.getElementById('Email');
-const Phone_Number = document.getElementById('Phone Number');
-const ID = document.getElementById('ID');
-const gpa = document.getElementById('GPA');
-const levels = document.getElementById('levels');
-const status = document.getElementById('Status');
-const department = document.getElementById('department');
-const Date = document.getElementById('Date');
-const Gender = document.getElementById('Gender');
+function validateForm() {
+    const Name = document.getElementById("Name").value;
+    const email = document.getElementById("Email").value;
+    const Phone = document.getElementById("Phone Number").value;
+    const id = document.getElementById("ID").value;
+    const gpa = document.getElementById("GPA").value;
+    var radio1 = document.getElementById("active");
+    var radio2 = document.getElementById("inactive");
+    var radio3 = document.getElementById("Male");
+    var radio4 = document.getElementById("Female");
+    var selectElement = document.getElementById("department");
+    var selectedOptionIndex = selectElement.selectedIndex;
+    var selectedOptionValue = selectElement.options[selectedOptionIndex].value;
+    var selectElement1 = document.getElementById("levels");
+    var selectedOptionIndex1 = selectElement1.selectedIndex;
+    var selectedOptionValue1 = selectElement1.options[selectedOptionIndex1].value;
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
 
-    validateInputs();
-});
 
-const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
+    if (Name === "") {
+        alert("name is required.");
+        return false;
+    } else if (!isValidName(Name)) {
+        alert("Please enter a valid name.")
+        return false;
+    }
 
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success')
+    if (email === "") {
+        alert("Email is required.");
+        return false;
+    } else if (!isValidEmail(email)) {
+        alert("Please enter a valid email address.");
+        return false;
+    }
+
+    if (id === "") {
+        alert("id is required.");
+        return false;
+    } else if (!isValid_id(id)) {
+        alert("Please enter a valid id.")
+        return false;
+    }
+
+    if (gpa === "") {
+        alert("gpa is required.");
+        return false;
+    } else if (!isValidGpa(gpa)) {
+        alert("Please enter a valid gpa.")
+        return false;
+    }
+
+    if (selectedOptionValue1 === "level 1") {
+
+    } else if (selectedOptionValue1 === "level 2") {
+
+    } else if (selectedOptionValue1 === "level 3") {
+    } else if (selectedOptionValue1 === "level 4") {
+
+    } else {
+        alert("No Level selected");
+        return false;
+    }
+
+    if (Phone === "") {
+        alert("email is required.");
+        return false;
+    } else if (isValidEmail(Phone)) {
+        alert("Please enter a valid phone number.");
+        return false;
+    }
+
+    if (radio1.checked) {
+
+    } else if (radio2.checked) {
+
+    } else {
+        alert("No Status selected");
+        return false;
+    }
+
+    if (selectedOptionValue === "CS") {
+
+    } else if (selectedOptionValue === "IS") {
+    } else if (selectedOptionValue === "IT") {
+
+    } else if (selectedOptionValue === "AI") {
+    } else if (selectedOptionValue === "DS") {
+
+    } else {
+        alert("No Department selected");
+        return false;
+    }
+
+    if (radio3.checked) {
+
+    } else if (radio4.checked) {
+
+    } else {
+        alert("No Gender selected");
+        return false;
+    }
+
+
+
+
+
+
+    alert("Student added successfully.");
+    return true;
 }
 
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-};
-
-const isValidEmail = Email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(Email).toLowerCase());
+function isValidEmail(email) {
+    // Basic email validation using regular expression
+    var emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+function isValidPhone(phone) {
+    var phoneRegex = /^[0-9]{11}$/;
+    return phoneRegex.test(phone);
+}
+function isValidName(Name) {
+    var NameRegex = /^[a-zA-Z ]+$/;
+    return NameRegex.test(Name);
 }
 
-const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = Email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
+function isValid_id(id) {
+    var Idregex = /^[0-9]{8}$/;
+    return Idregex.test(id);
+}
 
-    if (usernameValue === '') {
-        setError(username, 'Username is required');
-    } else {
-        setSuccess(username);
-    }
-
-    if (emailValue === '') {
-        setError(Email, 'Email is required');
-    } else if (!isValidEmail(emailValue)) {
-        setError(Email, 'Provide a valid email address');
-    } else {
-        setSuccess(Email);
-    }
-
-    if (passwordValue === '') {
-        setError(password, 'Password is required');
-    } else if (passwordValue.length < 8) {
-        setError(password, 'Password must be at least 8 character.')
-    } else {
-        setSuccess(password);
-    }
-
-    if (password2Value === '') {
-        setError(password2, 'Please confirm your password');
-    } else if (password2Value !== passwordValue) {
-        setError(password2, "Passwords doesn't match");
-    } else {
-        setSuccess(password2);
-    }
-
-};
+function isValidGpa(gpa) {
+    var Gparegex = /^([0-3].\d{1,2}|4.0)$/;
+    return Gparegex.test(gpa);
+}
