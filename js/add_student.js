4df466081c080a86f0804dd5f4ba4882d79313ce
Ahmed Ashraf <loaiashraf285@gmail.com>
2023-04-17 06:15:50 +0200

    modifying

---
+++ js/add_student.js
@@ -0,0 +1,78 @@
+const form = document.getElementById('form');
+const Name = document.getElementById('Name');
+const Email = document.getElementById('Email');
+const Phone_Number = document.getElementById('Phone Number');
+const ID = document.getElementById('ID');
+const gpa = document.getElementById('GPA');
+const levels = document.getElementById('levels');
+const status = document.getElementById('Status');
+const department = document.getElementById('department');
+const Date = document.getElementById('Date');
+const Gender = document.getElementById('Gender');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = Email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(Email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = Email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if (usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
+    }
+
+    if (emailValue === '') {
+        setError(Email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(Email, 'Provide a valid email address');
+    } else {
+        setSuccess(Email);
+    }
+
+    if (passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
+
+    if (password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+
+};
